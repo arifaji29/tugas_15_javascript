ff9f64fa33b246632f7ed6803bb31f0515463c91
Arif <arifajif@gmail.com>
2022-06-01 22:37:17 +0700

    tugas 15

---
+++ tugas15.js
@@ -0,0 +1,6 @@
+var ab = 8;
+console.log("nilai AB adalah", ab);
+var bc = 6;
+console.log("nilai BC adalah", bc);
+var ac = Math.sqrt(Math.pow(ab,2)+Math.pow(bc,2))
+console.log("panjang sisi AC adalah",ac)
